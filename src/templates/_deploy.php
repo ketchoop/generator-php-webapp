b9b72434b4b8f8ca666fc2611879dc6257afca3b
ket4yii <ket4yiit@gmail.com>
2016-11-20 23:44:21 +0300

    Update deployer version. Cleanup deployer configuration.

--- src/templates/_deploy.php
+++ src/templates/_deploy.php
@@ -1,11 +1,8 @@
 <?php
-//@todo set recipes directory in variable
 require 'recipe/composer.php';
-require '/var/www/html/vendor/deployphp/recipes/recipes/cachetool.php'; //add cachetool recipe installed via composer
 
 set('cachetool', '127.0.0.1:9000');
 set('repository', '<%= repo %>');
 
 serverlist('servers.yml');
 
-after('deploy:symlink', 'cachetool:clear:opcache');
