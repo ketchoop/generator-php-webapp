e13b065b4ac1d57d6f6fd946a0deebc68403efa1
ket4yii <ket4yiit@gmail.com>
2017-03-11 21:53:01 +0300

    Add cachetool for deployer

--- src/templates/_deploy.php
+++ src/templates/_deploy.php
@@ -1,8 +1,11 @@
 <?php
 require 'recipe/composer.php';
+require './vendor/deployer/recipes/cachetool.php'
 
 set('cachetool', '127.0.0.1:9000');
 set('repository', '<%= repo %>');
 
 serverlist('servers.yml');
+
+after('deploy:symlink', 'cachetool:clear:opcache');
 
